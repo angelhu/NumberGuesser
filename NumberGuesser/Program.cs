dfd3026b73c575e0d7bcc33b91f76f75f8a5599d
Angel Hu <angelqhu513@gmail.com>
2017-03-23 14:22:28 -0500

    Step 2

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -10,7 +10,14 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            /*  Generate a random number between 1 and 100
+                Prompt the user for a guess
+                If the guess is less than the program's number, it should tell you that you were low and let you guess again.
+                If the guess is greater than the program's number, it should tell you that you were high and let you guess again.
+                If the guess is correct, the program should tell you that you win and then quit.
+                After 5 incorrect guesses, the program should tell you that you lose.
+                You should display all the guesses to the screen */
+            Console.Write("Please guess a number between 1 and 100: "); 
         }
     }
 }
